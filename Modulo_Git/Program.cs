14792e39f059d3b8ae578f21d81cf43d19c519ee
Pierre <pierreoliveirajr78@gmail.com>
2022-12-09 19:29:46 -0300

    Criado Region Base

--- Modulo_Git/Program.cs
+++ Modulo_Git/Program.cs
@@ -13,9 +13,28 @@ namespace Modulo_Git
              * Crie um programa que receba dois valores e calcule a soma desses valores,
              * ao final exiba o resultado no console.
              */
+            #region Base
+            Console.WriteLine("Digite 1 para iniciar o programa ou digite 0 para sair");
+            int opcao = int.Parse(Console.ReadLine());
+            while (opcao != 0)
+            {
+                int a, b;
+                Console.WriteLine("Informe o valor dos numeros a serem somados");
+                a = int.Parse(Console.ReadLine());
+                b = int.Parse(Console.ReadLine());
+                int resultado = Soma(a, b);
+                Console.WriteLine("Soma dos valores é igual a " + resultado);
+                Console.WriteLine("Se desejar repetir digite 1 senão digite 0.");
+                opcao = int.Parse(Console.ReadLine());
+
+
+            }
+            #endregion
+
+            //Console.WriteLine("Informe o valor a ser somado");
+
         }
-        Console.WriteLine("Digite 1 para iniciar o programa e digite 0 para sair")
-        Console.WriteLine("Informe o valor a ser somado")
+        
 
         static int Soma(int a, int b)
         {
@@ -26,5 +45,7 @@ namespace Modulo_Git
         {
             return a - b;
         }
+
+        
     }
 }
