5aadbcacceac7d151d40c3a9e3031419ff53b9d5
Pierre <pierreoliveirajr78@gmail.com>
2022-12-09 18:46:59 -0300

    Criado comentario da atividade

---
+++ Modulo_Git/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modulo_Git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*Atividade:
+             * Crie um programa que receba dois valores e calcule a soma desses valores,
+             * ao final exiba o resultado no console.
+             */
+        }
+
+        static int Soma(int a, int b)
+        {
+            return a + b;
+        }   
+    }
+}
